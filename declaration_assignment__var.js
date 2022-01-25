0b0bf276a149dedb1f29b194c8f0fda444dd7bea
bernardogoltz <78585520+bernardogoltz@users.noreply.github.com>
2022-01-25 20:02:10 -0300

    Revisão data types

---
+++ declaration_assignment__var.js
@@ -0,0 +1,45 @@
+console.log("teste")
+
+// Variables and data types: 
+     // declaration
+var name 
+nameUser = 'Bernardo'
+
+//assignmnet
+console.log(nameUser)
+
+//checking data type:
+console.log(typeof nameUser)
+
+    let age, isHuman 
+
+    age = 18
+    isHuman = true
+
+    console.log(nameUser,age,isHuman)
+    console.log("O",nameUser,"tem",age,"anos")
+
+    //interpolling values
+    console.log(`O ${nameUser} tem ${age} anos`)
+
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+console.log(person.name)
+console.log(`O ${person.name} tem ${person.age} anos`)
+
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat'
+]
+
+console.log(animals)
+console.log(animals[0])
+console.log(animals.length)
+
+    
